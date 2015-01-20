638b37b9d39f61fb17296775492125a5fb2293f5
unknown <2088209l@BO715-2-03.ad.dcs.gla.ac.uk>
2015-01-20 12:48:21 +0000

    Chapter4 - completed

--- knlazarova/rango/views.py
+++ knlazarova/rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index (request):
-    return HttpResponse ("Rango says hey there world! :) ")
+    return HttpResponse ("Rango says hey there world! :) <br/> <a href='/rango/about'>About</a> ")
 
 def about (request):
-    return HttpResponse ("Rango says here is the about page ^.^")
+    return HttpResponse ("Rango says here is the about page ^.^ <a href='/rango/'>Index</a> <br/> This tutorial has been put together by Kristina Lazarova: 2088209l </br>")
