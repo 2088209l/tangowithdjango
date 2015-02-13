5e64309a8bba2eacc81a853ab71f7825aca2ab9f
Kristina Lazarova <2088209l@student.gla.ac.uk>
2015-02-13 00:23:50 +0000

    Chapter 11 - Completed

--- knlazarova/rango/views.py
+++ knlazarova/rango/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Import the Category and Page model
-from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm
-from rango.forms import PageForm
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
-from rango.forms import UserForm, UserProfileForm
+from datetime import datetime
 
 def index (request):
     # Query the database for a list of ALL categories currently stored.
@@ -21,14 +18,43 @@ def index (request):
     # Retrieve top 5 viewed pages
     pages_dict = Page.objects.order_by('-views')[:5]
     context_dict['top5pages'] = pages_dict
-    
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context_dict)
-    # return HttpResponse ("Rango says hey there world! :) <br/> <a href='/rango/about'>About</a> ") - "About"
 
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+    response = render(request,'rango/index.html', context_dict)
+
+    return response
+    
 def about (request):
     context_dict = {'boldmessage': "Chapter 5 is almost done :)"}
-    return render(request, 'rango/about.html', context_dict)
+    # If the visits session varible exists, take it and use it.
+    # If it doesn't, we haven't visited the site so set the count to zero.
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+    return render(request, 'rango/about.html', {'visits': count})
 
 def category(request, category_name_slug):
 
